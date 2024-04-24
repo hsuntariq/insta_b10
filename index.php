3614cf207621a947513ce9bd49d51f9a170664d0
unknown <hsuntariq@gmail.com>
2024-04-24 12:04:32 +0500

    initial commit

---
+++ index.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <?php include './boot_css.php' ?>
+    <title>Document</title>
+    <style>
+        .images img{
+            width: 51.5%;
+            top:4%;
+            right:14%
+        }
+    </style>
+</head>
+<body>
+    
+
+    <div class="container">
+        <div class="row col-lg-9 mx-auto">
+            <div class="col-lg-6 position-relative">
+                <img width="100%" src="https://static.cdninstagram.com/images/instagram/xig/homepage/phones/home-phones.png?__makehaste_cache_breaker=HOgRclNOosk" alt="">
+                <div class="images">
+                    <img  class="position-absolute" src="./assets/screenshot1.png" alt="">
+                    <img  class="position-absolute" src="./assets/screenshot2.png" alt="">
+                    <img  class="position-absolute" src="./assets/screenshot3.png" alt="">
+                    <img   class="position-absolute" src="./assets/screenshot4.png" alt="">
+                </div>
+            </div>
+            <div class="col-lg-6">
+            </div>
+        </div>
+    </div>
+
+
+
+</body>
+</html>
